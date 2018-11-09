e9cf11fec4fcf8a1ab4d01c82fb0b27758099b1d
C7Meteors <43677534+C7Meteors@users.noreply.github.com>
2018-11-08 19:49:50 -0600

    Update RollDice.cs

--- RollDice.cs
+++ RollDice.cs
@@ -8,40 +8,10 @@ namespace DnDice
 {
     class RollDice
     {
-        public void Rolld4()
+        public void Rolld(int dice)
         {
             Random value = new Random();
-            Console.WriteLine(value.Next(1, 4));
-        }
-
-        public void Rolld6()
-        {
-            Random value = new Random();
-            Console.WriteLine(value.Next(1, 6));
-        }
-
-        public void Rolld8()
-        {
-            Random value = new Random();
-            Console.WriteLine(value.Next(1, 8));
-        }
-
-        public void Rolld10()
-        {
-            Random value = new Random();
-            Console.WriteLine(value.Next(1, 10));
-        }
-
-        public void Rolld12()
-        {
-            Random value = new Random();
-            Console.WriteLine(value.Next(1, 12));
-        }
-
-        public void Rolld20()
-        {
-            Random value = new Random();
-            Console.WriteLine(value.Next(1, 20));
+            Console.WriteLine(value.Next(1, dice));
         }
     }
 }
